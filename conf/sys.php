69a6067805c84860c15c20dfff722f0c75043d9c
Thiago Cassimiro <43118264+woolz@users.noreply.github.com>
2018-10-10 16:07:15 -0300

    Create sys.php

---
+++ conf/sys.php
@@ -0,0 +1,6 @@
+<?php
+    $sys_name = "Secure PHP Login"; // Website name
+    $sys_url = "http://localhost/phplogin"; // Website url
+    $sys_base = "/var/www/html/phplogin" // System files location
+    $sys_favicon = $sys_url . "/images/favicon.ico"; // Website favicon
+    $sys_logo = $sys_url . "/images/logo.png"; // Website logo
